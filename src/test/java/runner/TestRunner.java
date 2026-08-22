73ea0a5f9555e25c9a5b4f64873ea0ad006157e0
DavidSafta <david_safta@outlook.com>
2026-08-22 14:19:32 +0300

    Add Allure reporting: allure-cucumber7-jvm plugin integration

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        plugin = {"pretty"}
+        plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
 )
 public class TestRunner {
 }
